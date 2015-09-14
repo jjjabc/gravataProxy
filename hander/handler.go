96818170b8cc35134d8146780e8380e3be93b0d2
jiaxun <jjjabc123@gmail.com>
2015-09-14 15:51:46 +0800

    增加非法url判断

--- hander/handler.go
+++ hander/handler.go
@@ -3,6 +3,7 @@ package hander
 import (
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func Proxy(w http.ResponseWriter, re *http.Request) {
@@ -12,11 +13,26 @@ func Proxy(w http.ResponseWriter, re *http.Request) {
 	if proxyRequest.URL.Scheme == "" {
 		proxyRequest.URL.Scheme = "http"
 	}
+	/*	if re.URL.Scheme != "http" || re.URL.Scheme != "https" {
+		return
+	}*/
+	isPathOK := strings.HasPrefix(re.URL.Path, "/avatar/")
+	if len(re.URL.Path) == 40 {
+		isPathOK = isPathOK && true
+	} else if (len(re.URL.Path) == 41) && re.URL.Path[40] == '/' {
+		isPathOK = isPathOK && true
+	}else{
+		isPathOK = false
+	}
+	if !isPathOK {
+		http.NotFound(w, re)
+		return
+	}
 	proxyRequest.URL.Host = "gravatar.com"
-	proxyRequest.RequestURI = ""
+	proxyRequest.RequestURI = "" // net/http/request.go L216:It is an error to set this field in an HTTP client request.
 	response, err := client.Do(&proxyRequest)
 	if err != nil {
-		w.Write([]byte("client.Do error:" + err.Error()))
+		http.Error(w, "client.Do error:"+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -24,7 +40,7 @@ func Proxy(w http.ResponseWriter, re *http.Request) {
 	defer bodyRC.Close()
 	data, err := ioutil.ReadAll(bodyRC)
 	if err != nil {
-		w.Write([]byte("read response body error:" + err.Error()))
+		http.Error(w, "read response body error:"+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Write(data)
